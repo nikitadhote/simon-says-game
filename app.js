6def9ea2e669b1e4bf80360d57065c86bc16617e
Nikita Dhote <nikitadhote13@gmail.com>
2026-06-19 17:38:04 +0530

    Fixed score spacing in game over message

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ function checkAns(idx) {
         }
         
     } else {
-       h2.innerHTML = `Game Over! Your score was<b>${level}</b> <br>Press any key to start`;
+       h2.innerHTML = `Game Over! Your score was<b>${level}</b><br>Press any key to start`;
        document.querySelector("body").style.backgroundColor="red";
        setTimeout(function() {
           document.querySelector("body").style.backgroundColor = "white";
